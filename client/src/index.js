a41a079786737e159d5a859fdf93945419f59260
Michel Ventura <michcodes@hotmail.com>
2023-03-22 14:26:09 -0600

    feat: redux config

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import messageReducer from "./services/messageSlice";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+const store = configureStore({
+  reducer: {
+    messages: messageReducer,
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );

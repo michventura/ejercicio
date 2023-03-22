6f79638f0c7074d01657de807f5d5ebc13cce509
Michel Ventura <michcodes@hotmail.com>
2023-03-22 14:27:48 -0600

    feat: display messages that persist refresh

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,84 @@
+import * as React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { addMessage, highlightMessage, deleteMessage } from "./services";
+import { useLocalStorage } from "./hooks";
+
 function App() {
+  const dispatch = useDispatch();
+  const [storedMessages, setStoredMessages] = useLocalStorage("messages", []);
+  const { messages } = useSelector((state) => ({
+    messages: storedMessages,
+  }));
+  const [message, setMessage] = React.useState("");
+
+  const handleAddMessage = (e) => {
+    e.preventDefault();
+    if (!message) {
+      return;
+    }
+    dispatch(addMessage(message));
+    setStoredMessages([
+      ...storedMessages,
+      { id: Date.now(), body: message, highlighted: false },
+    ]);
+    setMessage("");
+  };
+
+  const handleHighlightMessage = (id) => {
+    dispatch(highlightMessage(id));
+    setStoredMessages(
+      storedMessages.map((message) =>
+        message.id === id
+          ? { ...message, highlighted: !message.highlighted }
+          : message
+      )
+    );
+  };
+
+  const handleDeleteMessage = (id) => {
+    dispatch(deleteMessage(id));
+    setStoredMessages(storedMessages.filter((message) => message.id !== id));
+  };
+
   return (
-      <p>React app</p>
+    <div className="container">
+      <h1>📝 Notas</h1>
+      <form onSubmit={handleAddMessage}>
+        <input
+          type="text"
+          onChange={(e) => setMessage(e.target.value)}
+          value={message}
+          required
+        />
+        <button type="submit" disabled={!message}>
+          ️💾
+        </button>
+      </form>
+      {messages.length === 0 && <p className="empty">No hay notas...</p>}
+      <ul>
+        {[...messages].reverse().map((message) => (
+          <li key={message.id}>
+            <input
+              className="highlight-checkbox"
+              type="checkbox"
+              checked={message.highlighted}
+              onChange={() => handleHighlightMessage(message.id)}
+            />
+            {message.highlighted ? (
+              <span className="highlight">{message.body}</span>
+            ) : (
+              message.body
+            )}
+            <button
+              className="delete-button"
+              onClick={() => handleDeleteMessage(message.id)}
+            >
+              🗑️️
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
